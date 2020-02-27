f5aaeded630907776b31b206cbcddcb333273296
nguyenkhanhtung <tungnk0711@gmail.com>
2020-02-27 16:34:47 +0700

    commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace IsPrime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 2;
+            bool check = true;
+            int number;
+
+            Console.WriteLine("Enter a number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            while (i <= Math.Sqrt(number))
+            {
+                if (number % i == 0)
+                {
+                    check = false;
+                    break;
+                }
+                i++;
+            }
+
+            if (check)
+                Console.WriteLine(number + " is a prime.");
+            else
+                Console.WriteLine(number + " is not a prime.");
+        }
+    }
+}
